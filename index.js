68b36b64ac08e941d53a36d4110f253ea4fc02ce
PabloT <tavarespablo7@gmail.com>
2020-10-14 23:12:12 -0600

    Creacion del servidor en el puerto 5000

---
+++ index.js
@@ -0,0 +1,19 @@
+// Importar express
+const express = require("express");
+
+// Crear un servidor express
+const app = express();
+
+//crear ruta para /
+app.get("/", (req, res, next) => {
+    res.send("Bienvenidos");
+  });
+
+
+// Inicializar el servidor en un puerto en específico
+app.listen(5000, () => {
+    console.log("Servidor ejecutándose en el puerto 5000");
+  });
+
+
+  
